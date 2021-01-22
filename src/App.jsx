2b127606d376aaffa032be323aa9e60afa97717b
lkripa <lriparip@gmail.com>
2021-01-22 15:30:13 +0100

    cleaned up order

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ import { greatCircle, point } from '@turf/turf';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibGtyaXBhIiwiYSI6ImNrazVpZHQ5OTBxa3kyd3FuMnoyYmVlZHAifQ.wxeraMVYC8zmS4rXERn4ng';
 
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -20,7 +19,6 @@ class App extends React.Component {
       lng: 5,
       lat: 34,
       zoom: 2
-      
     };
     this.myRef = React.createRef();
   }
@@ -32,11 +30,28 @@ class App extends React.Component {
       center: [this.state.lng, this.state.lat],
       zoom: this.state.zoom
     });
-
+    // Start Locatiions and Destination Point
     var start1 = point([-3.7025600, 40.4165000]); // Madrid
     var start2 = point([8.5500000, 47.3666700]); // Zurich
     var end = point([9.283447, 40.078072]); // Sardinia
     
+    // Set loading map view
+    map.on('move', () => {
+      this.setState({
+      lng: map.getCenter().lng.toFixed(4),
+      lat: map.getCenter().lat.toFixed(4),
+      zoom: map.getZoom().toFixed(2)
+      });
+    });
+  
+    map.on('load', () => {
+      map.addSource('places', {
+        type: 'geojson',
+        data: startLocations & destinations
+      });
+      addMarkers();
+      drawLines(end);
+    });
     function drawLines(end){
       var greatCircle1 = greatCircle(start1, end, {'name': 'Madrid to Sardinia'});
       var greatCircle2 = greatCircle(start2, end, {'name': 'Zurich to Sardinia'});
@@ -48,7 +63,6 @@ class App extends React.Component {
           'features': [ greatCircle1, greatCircle2 ]
         }
       });
-
       map.addLayer({
         'id': 'flight_lines',
         'type': 'line',
@@ -62,84 +76,47 @@ class App extends React.Component {
         }
       });
     }
-    
-    map.on('move', () => {
-      this.setState({
-      lng: map.getCenter().lng.toFixed(4),
-      lat: map.getCenter().lat.toFixed(4),
-      zoom: map.getZoom().toFixed(2)
+
+    function addMarkers() {
+      // Start location marker load
+      /* For each feature in the GeoJSON object above: */
+      startLocations.features.forEach(function(marker) {
+        /* Create a div element for the marker. */
+        var el = document.createElement('div');
+        /* Assign a unique `id` to the marker. */
+        el.id = "marker-" + marker.properties.id;
+        /* Assign the `marker` class to each marker for styling. */
+        el.className = 'marker';
+        el.addEventListener('click', () => { 
+          console.log("Start Location Clicked.");
+        }); 
+
+        new mapboxgl.Marker(el, { offset: [0, -23] })
+          .setLngLat(marker.geometry.coordinates)
+          .addTo(map);
       });
-    });
-  
-    map.on('load', () => {
-      // var start1 = point([-3.7025600, 40.4165000]); // Madrid
-      // var start2 = point([8.5500000, 47.3666700]); // Zurich
-      // var end = point([9.283447, 40.078072]); // Sardinia
-      // var greatCircle1 = greatCircle(start1, end, {'name': 'Madrid to Sardinia'});
-      // var greatCircle2 = greatCircle(start2, end, {'name': 'Zurich to Sardinia'});
+      // Destination marker Load
+      destinations.features.forEach(function(marker) {
+        /* Create a div element for the marker. */
+        var ed = document.createElement('div');
+        /* Assign a unique `id` to the marker. */
+        ed.id = "marker-" + marker.properties.id;
+        /* Assign the `marker` class to each marker for styling. */
+        ed.className = 'destination_marker';
+        ed.addEventListener('click', e => 
+        { 
+          // Reload the newly clicked destination to show flight lines
+          end = marker.geometry.coordinates;
+          map.removeLayer('flight_lines')
+          map.removeSource('flight_lines')
+          drawLines(end);
+        });
 
-      map.addSource('places', {
-        type: 'geojson',
-        data: startLocations & destinations
+        new mapboxgl.Marker(ed, { offset: [0, -23] })
+          .setLngLat(marker.geometry.coordinates)
+          .addTo(map);
       });
-
-      addMarkers();
-      drawLines(end);
-      // map.on('click', Marker, e => {
-      //   var locationFeatures = e.point;
-      //   console.log("click", locationFeatures);
-      // });
-    });
-
-  function addMarkers() {
-    /* For each feature in the GeoJSON object above: */
-    startLocations.features.forEach(function(marker) {
-      /* Create a div element for the marker. */
-      var el = document.createElement('div');
-      /* Assign a unique `id` to the marker. */
-      el.id = "marker-" + marker.properties.id;
-      /* Assign the `marker` class to each marker for styling. */
-      el.className = 'marker';
-      el.addEventListener('click', () => 
-   { 
-      console.log("Start Location Clicked.");
-   }
-); 
-      
-      /**
-       * Create a marker using the div element
-       * defined above and add it to the map.
-      **/
-      new mapboxgl.Marker(el, { offset: [0, -23] })
-        .setLngLat(marker.geometry.coordinates)
-        .addTo(map);
-    });
-
-    destinations.features.forEach(function(marker) {
-      /* Create a div element for the marker. */
-      var ed = document.createElement('div');
-      /* Assign a unique `id` to the marker. */
-      ed.id = "marker-" + marker.properties.id;
-      /* Assign the `marker` class to each marker for styling. */
-      ed.className = 'destination_marker';
-      ed.addEventListener('click', e => 
-      { 
-         end = marker.geometry.coordinates; // Toyko
-         map.removeLayer('flight_lines')
-         map.removeSource('flight_lines')
-         drawLines(end);
-      });
-       
-      /**
-       * Create a marker using the div element
-       * defined above and add it to the map.
-      **/
-      new mapboxgl.Marker(ed, { offset: [0, -23] })
-        .setLngLat(marker.geometry.coordinates)
-        .addTo(map);
-    });
-  }
-
+    }
 }
 
   render() {
